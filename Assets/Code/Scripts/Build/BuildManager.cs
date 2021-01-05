31f3d52604362fd346b94e6d6d4c84adedae3450
Daniel Bäcker <baecker.daniel1995@gmail.com>
2021-01-05 17:15:50 +0100

    Build-System

--- Assets/Code/Scripts/Build/BuildManager.cs
+++ Assets/Code/Scripts/Build/BuildManager.cs
@@ -3,7 +3,9 @@
 // Project  : Projekt-Klopapier
 
 
+using System;
 using Buildings;
+using Code.Scripts;
 using UI_Scripts;
 using UnityEngine;
 
@@ -17,13 +19,14 @@ namespace Build
        
         
         private bool IsBuilding = false;
+        private bool CanBuild;
         public bool GetIsBuilding => IsBuilding;
         
         private GameObject CurrentBuilding;
         
         private void Awake()
         {
-            BuildSlot.OnMouseClick += CreateBuilding;
+            BuildSlot.OnMouseClick += OnMouseClickedBuildSlot;
         }
 
         private void Start()
@@ -31,74 +34,182 @@ namespace Build
             IsHudOpenH = false;
         }
 
-        private void CreateBuilding(BuildingData buildingData)
+        private void OnMouseClickedBuildSlot(BuildingData buildingData)
         {
             if (IsBuilding)
             {
-                Destroy(CurrentBuilding);
-                CurrentBuilding = null;
+                DestroyCurrentBuilding();
             }
+            
+            CreateBuilding(buildingData);
+            IsBuilding = true;
+        }
+
+        private void DestroyCurrentBuilding()
+        {
+            Destroy(CurrentBuilding);
+            CurrentBuilding = null;
+        }
+
+        private void CreateBuilding(BuildingData buildingData)
+        {
             CurrentBuilding = Instantiate(BuildingPrefab);
             CurrentBuilding.GetComponent<Building>().Init(buildingData);
-            IsBuilding = true;
         }
 
         public void BuildBuilding()
         {
+            if (!IsBuilding) return;
+
+            foreach (Street street in Street.Streets)
+            {
+                street.SetAllArrowsActive(true);
+            }
+            
             Vector3 mousePos = Input.mousePosition;
             
-            if (IsBuilding && mousePos.x > 0 && mousePos.x < Screen.width && mousePos.y > 0 && mousePos.y < Screen.height)
+            if (mousePos.x > 0 && mousePos.x < Screen.width && mousePos.y > 0 && mousePos.y < Screen.height)
             {
-                
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(mousePos);
-                if (  Physics.Raycast(ray, out hit) )
-                {
-                    bool isPlane = true;
-                    
-                    while (hit.transform.gameObject.name != "Plane")
-                    {
-                        isPlane = false;
-                        Vector3 cameraDirection = Camera.main.transform.forward;
-                        ray = new Ray(hit.point + cameraDirection * 0.01f,cameraDirection);
-                        RaycastHit hit2;
-                        if (!Physics.Raycast(ray, out hit2))
-                        {
-                            break;
-                        }
-//
-                        if (hit2.transform.name == "Plane")
-                        {
-                            isPlane = true;
-                        }
-                        hit = hit2;
-                    }
-                    
-                    if (isPlane)
-                    {
-                        CurrentBuilding.transform.position = hit.point;
-                        if (CurrentBuilding.GetComponent<Building>().IsCollison)
-                        {
-                            CurrentBuilding.GetComponent<Building>().SetCantBuildMaterial();
-                        }
-                        else
-                        {
-                            CurrentBuilding.GetComponent<Building>().SetBuildMaterial();
-                        }
-                    }
-                }
-
-                if (Input.GetMouseButtonDown(0) && !CurrentBuilding.GetComponent<Building>().IsCollison)
-                {
-                    IsBuilding = false;
-                    CurrentBuilding.GetComponent<Building>().SetBuildingMaterial();
-                    BuildUI.SetActive(false);
-                    
-                }
+                SetBuildingToMousePos();
+                HandleMouseInput();
             }
         }
 
-        public void OnButton_Buildmenue()
+        private void SetBuildingToMousePos()
+        {
+            Vector3 mousePos;
+            Vector3 cameraDirection;
+            RaycastHit hit;
+           
+            Ray ray;
+            Ground ground;
+            bool isHit;
+            bool isGround;
+
+            cameraDirection = Camera.main.transform.forward;
+            mousePos = Input.mousePosition;
+            ray = Camera.main.ScreenPointToRay(mousePos);
+            isHit = Physics.Raycast(ray, out hit);
+            
+            if (isHit)
+            {
+                isGround = true;
+                ground = hit.transform.GetComponent<Ground>();
+                while (ground == null)
+                {
+                    isGround = false;
+                    RaycastHit nextHit;
+                    ray = new Ray(hit.point + cameraDirection * 0.01f,cameraDirection);
+                    isHit = Physics.Raycast(ray, out nextHit);
+                    if (!isHit) break;
+                    
+                    hit = nextHit;
+                    
+                    ground = hit.transform.GetComponent<Ground>();
+                    if (ground != null)
+                    {
+                        isGround = true;
+                    }
+                }
+                
+                if (isGround)
+                {
+                    int index = hit.triangleIndex;
+                    CurrentBuilding.transform.position = hit.point;
+
+                    EGround groundSignature = ground.GetGroundSignature;
+
+                    switch (groundSignature)
+                    {
+
+                        case EGround.Street:
+                            CurrentBuilding.GetComponent<Building>().SetBuildMaterial();
+                            Street street = ground as Street;
+                            
+                            CanBuild = true;
+
+                            switch (index)
+                            {
+                                case 0:
+                                    if (street.GetIsTopStreet)
+                                    {
+                                        CanBuild = false;
+                                    }
+                                    else
+                                    {
+                                        CurrentBuilding.transform.position = street.GetPosTop;
+                                    }
+                                    break;
+                                case 1:
+                                    if (street.GetIsRightStreet)
+                                    {
+                                        CanBuild = false;
+                                    }
+                                    else
+                                    {
+                                        CurrentBuilding.transform.position = street.GetPosRight;
+                                    }
+
+                                    break;
+                                case 2:
+                                    if (street.GetIsLeftStreet)
+                                    {
+                                        CanBuild = false;
+                                    }
+                                    else
+                                    {
+                                        CurrentBuilding.transform.position = street.GetPosLeft;
+                                    }
+
+                                    break;
+                                case 3:
+                                    if (street.GetIsDownStreet)
+                                    {
+                                        CanBuild = false;
+                                    }
+                                    else
+                                    {
+                                        CurrentBuilding.transform.position = street.GetPosDown;
+                                    }
+
+                                    break;
+                                default:
+                                    break;
+                            }
+                            
+                            
+                            
+                            break;
+                        default:
+                            CurrentBuilding.GetComponent<Building>().SetCantBuildMaterial();
+                            CanBuild = false;
+                            break;
+                    }
+
+                    if (!CanBuild)
+                    {
+                        CurrentBuilding.GetComponent<Building>().SetCantBuildMaterial();
+                    }
+                }
+            }
+        }
+        
+        private void HandleMouseInput()
+        {
+            if (Input.GetMouseButtonDown(0) && !CurrentBuilding.GetComponent<Building>().IsCollison && CanBuild)
+            {
+                IsBuilding = false;
+                CurrentBuilding.GetComponent<Building>().SetBuildingMaterial();
+                BuildUI.SetActive(false);
+                foreach (Street street in Street.Streets)
+                {
+                    street.SetAllArrowsActive(false);
+                }
+            }
+            
+        }
+
+        public void OnButton_BuildMenu()
         {
             IsHudOpenH = true;
         }
---
+++ Assets/Code/Scripts/Ground.cs
@@ -0,0 +1,42 @@
+﻿// File     : Ground.cs
+// Author   : Daniel Bäcker
+// Project  : Projekt-Klopapier
+
+using System;
+using UnityEngine;
+
+namespace Code.Scripts
+{
+
+    public enum EGround
+    {
+        None,
+        Street,
+        Gras,
+    }
+    
+    public class Ground : MonoBehaviour
+    {
+        public static Ground[,] Grounds;
+
+        public static void SetGroundSize(int _width, int _height)
+        {
+            Grounds = new Ground[_width, _height];
+        }
+
+        private int Width;
+        private int Height;
+
+        [SerializeField]
+        private EGround GroundSignature;
+
+        public EGround GetGroundSignature => GroundSignature;
+
+
+        public void Init(int _widthPos, int _heightPos)
+        {
+            Grounds[_widthPos, _heightPos] = this;
+        }
+
+    }
+}
