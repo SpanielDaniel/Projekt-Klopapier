db4bcf42e82c7a1e4e22658fef688a936b7dd80d
Daniel <daniel.pobijanski@outlook.com>
2021-02-28 19:18:36 +0100

    Turrets in House

---
+++ Assets/Code/Scripts/Unit Scripts/RayDetection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayDetection : MonoBehaviour
+{
+    private float Range;
+    public bool IsHit;
+    private UnitWeapon HouseWeapon;
+
+    private void Start()
+    {
+        HouseWeapon = GetComponentInParent<UnitWeapon>();
+        Range = HouseWeapon.DetectionRange;
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Range))
+        {
+            if (hit.transform.tag == "Enemy")
+            {
+                HouseWeapon.GetComponent<UnitWeapon>().GetTarget(hit.transform.gameObject);
+                IsHit = true;
+            }
+            else
+            {
+                IsHit = false;
+            }
+        }
+    }
+}
